f6ddeb6824e619052b4cb21f26592ee8b31b93f4
me <a@a.com>
2020-06-30 17:40:22 +0800

    probable correct

--- project.cpp
+++ project.cpp
@@ -3,7 +3,7 @@
 /*
 @constructor
 */
-Student::Student(string name, string academy, string cllass, string major, Card* card = NULL, int id) {
+Student::Student(string name, string academy, string cllass, string major, int id) {
 	_name = name;
 	_academy = academy;
 	_class = cllass;
@@ -11,72 +11,126 @@ Student::Student(string name, string academy, string cllass, string major, Card*
 	_id = id;	
 }
 
+~Student() {
+    for (auto i = students.begin(); i != student.end(); i++) {
+        if ((*i).get_stu_id() == _stu_id) {
+            students.erase(i);
+            for (auto j = cards.begin(); j != cards.end(); j++) {
+                if ((*j).get_card_id() == _card_id ) {
+                    cards.erase(j);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+}
 /*
-*@充值成功，卡没有挂失时，即充值金额大于0小于等于500返回true，否则返回false
+*@充值成功，卡没有挂失时，即充值金额大于i0小于等于500返回true，否则返回false
 */
-void Student::recharge(int amount) {
+void Student::recharge(double amount) {
 	_card->recharge(amount);
 }	
 
 /*
 *@卡没有挂失时，消费金额大于0小于等于500并且小于当前余额返回true，否则返回false
 */
-void Student::Student::expense(int amount) {
+void Student::Student::expense(double amount) {
 	_card->expense(amount);
 }
 
 /*
 *@查看自己校园卡的状态
 */
-bool Student::check_status() {
-return 
+void Student::check_status() {
+	if (find_card()->get_flag() == GET_STATE) {
+		puts("您的卡现在处于正常状态！");
+	}
+	else {
+		puts("您的卡现在处于挂失状态！");
+	}
 }					
 
 /*
 *@挂失，不能挂失已经挂失的卡
 */
-bool Student::loss() {
-	
+void Student::loss() {
+	find_card->set_flag(LOSS_STATE);
 }						
 
 /*
 *@找到了卡，不能卡在自己手上但是又找到了
 */
-bool Student::find() {
-
+void Student::find() {
+	find_card()->set_flag(GET_STATE);
 }
 
 /*
 *@重新补办卡
 */
-void Student::replace();
+void Student::replace() {
+	find();
+}
 
 /*
 *@查看充值记录
 */
-void Student::check_charge_record();
+void Student::check_charge_record() {
+	puts("时间\t\t充值金额");
+	for (auto i : find_card()->get_charge_record()) {
+		tm *ltm = localtime(&i.now);
+		printf("%d-%d-%d %d:%d:%d\t\t%.2lf\n",ltm->tm_year, ltm->tm_mon, ltm->tm_mday,
+											  ltm->tm_hour, ltm->tm_min, ltm->tm.sec, i.amount); 
+	}
+}
 
 /*
 *@查看消费记录
 */
-void Student::check_expense_record();
+void Student::check_expense_record() {
+	puts("时间\t\t消费金额");
+	for (auto i : find_card()->get_consume_record()) {
+		tm *ltm = localtime(&i.now);
+		printf("%d-%d-%d %d:%d:%d\t\t%.2lf\n",ltm->tm_year, ltm->tm_mon, ltm->tm_mday,
+											  ltm->tm_hour, ltm->tm_min, ltm->tm.sec, i.amount); 
+	}
+}
+
 /*
 *@学生和卡绑定
 */
-void Student::bind_card();
+void Student::bind_card(Card c) {
+	_card_id = get_card_id(c);
+	c.bind_stu(*this);
+}
 
 /*
 *@
 */
-int Student::get_id();
+int Student::get_stu_id() {
+	return _stu_id;
+}
 
 /*
 *@
 */
-Card::Card(int id, int remain = 0, string password, bool flag = 0, Student* stu = NULL); {
+Card* Student::find_card() {
+	for (auto i = cards.begin(); i != cards.end(); i++) {
+		if ((*i).get_card_id() == _card_id) {
+			return &(*i);
+		}
+	}
+	return NULL;
+}
+
+/*
+*@
+*/
+Card::Card(int id, double remain = 0, string password, bool flag = 0); {
 	this->_id = id;
 	this->_remain = remain;
 	this->_password = password; 
+	this->_flag = flag;
 }
 
 /*
@@ -89,24 +143,67 @@ Card::~Card() {
 /*
 *@充值
 */
-bool Card::recharge(int amount) {
+void Card::recharge(double amount) {
+	if (_flag == LOSS_STATE) {
+		puts("抱歉，此卡已被冻结！");
+		return;
+	}
+	
+	if (amount < 0 || amount >500) {
+		puts("充值金额应为0-500！ ");
+		return;
+	}
+	
 	this->_remain += amount;
-	_charge.
+	record tmp;
+	tmp.now = time(0);
+	tmp.amount = amount;
+	_charge.emplace_back(tmp);
 }
 
 /*
 *@消费
 */
-bool Card::expense(int amount) {
-	this->_remain -= amount;
+void Card::expense(double amount) {
+	if (_flag == LOSS_STATE) {
+		puts("抱歉，此卡已被冻结！");
+	}
+	else if (amount > this->_remain) {
+		puts("抱歉，您的余额不足！");
+	}
+	else if (amount <= 0) {
+		puts("抱歉，消费金额必须为正数");
+	}
+	else {
+		this->_remain -= amount;
+		record tmp;
+		tmp.now = time(0);
+		tmp.amount = amount;
+		_consume.emplace_back(tmp);
+	}
 }
 
 /*
 *@挂失与解挂失传入的这个参数和当前_flag相反时才能设置成功
 */
-bool Card::set_flag(int flag) {
-	if (flag == !this->_flag) {
-		this->_flag = flag;
+void Card::set_flag(int flag) {
+	if (flag == LOSS_STATE) {
+		if (this->_flag == LOSS_STATE) {
+			puts("您的卡已经处于挂失状态！");
+		}
+		else {
+			this->_flag = LOSS_STATE;
+			puts("您的卡成功挂失！期间无法进行消费，充值。");
+		}
+	}
+	else {
+		if (this->_flag == GET_STATE) {
+			puts("您的卡当前处于正常状态");
+		}
+		else {
+			this->_flag = GET_STATE;
+			puts("您的卡成功解除挂失！可以继续消费，充值。");
+		}
 	}
 }
 
@@ -127,20 +224,20 @@ bool Card::get_flag() {
 /*
 *@卡和学生绑定
 */
-void Card::bind_stu() {
-
+void Card::bind_stu(Student s) {
+	_stu_id = s.get_stu_id();
 }
 
 /*
 *@返回充值记录
 */
 vector<record> Card::get_charge_record() {
-	
+	return _charge;
 }
 
 /*
 *@返回消费记录
 */
 vector<record> Card::get_consume_record() {
-
+	return _cousume;
 }
--- project.h
+++ project.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <string>
 #include <vector>
 #include <ctime>
 using namespace std;
@@ -13,15 +14,15 @@ using namespace std;
 
 typedef struct record {
 	time_t now;
-	int amount;
+	double amount;v
 } record;
 
 class Student {
 public:
-	Student(string name, string academy, string cllass, string major, Card* card = NULL, int id);
+	Student(string name, string academy, string cllass, string major, int id);
 	~Student();						//退学处理
-	void recharge(int amount);		//充值成功，卡没有挂失时，即充值金额大于0小于等于500返回true，否则返回false
-	void expense(int amount);		//卡没有挂失时，消费金额大于0小于等于500并且小于当前余额返回true，否则返回false
+	void recharge(double amount);		//充值成功，卡没有挂失时，即充值金额大于0小于等于500返回true，否则返回false
+	void expense(double amount);		//卡没有挂失时，消费金额大于0小于等于500并且小于当前余额返回true，否则返回false
 	bool check_status();					//查看自己校园卡的状态
 	bool loss();						//挂失，不能挂失已经挂失的卡
 	bool find();						//找到了卡，不能卡在自己手上但是又找到了
@@ -29,39 +30,43 @@ public:
 	void check_charge_record();			//查看充值记录
 	void check_expense_record();			//查看消费记录
 	void bind_card();						//学生和卡绑定
-	int get_id();
+	Card* find_card();
+	int get_stu_id();
 
 private:
-	Card* _card;
+	int _card_id = -1;
 	string _name, _academy, _class, _major;
-	int _id;
+	int _stu_id;
 	//名字，学院，班级，专业，学号
 };
 
 class Card {
 public:
-	Card(int id, int remain = 0, string password, bool flag = 0, Student* stu = NULL);
+	Card(int id, double remain = 0, string password, bool flag = 0);
 	~Card();							//卡不想要了
-	bool recharge(int amount);			//充值
-	bool expense(int amount);			//消费
+	void recharge(double amount);			//充值
+	void expense(double amount);			//消费
 	bool set_flag(int flag);			//挂失与解挂失传入的这个参数和当前_flag相反时才能设置成功
 	int get_remain();					//返回余额
 	bool get_flag();					//返回挂失状态
 	void bind_stu();						//卡和学生绑定
+	int get_card_id()
 	vector<record> get_charge_record();	//返回充值记录
 	vector<record> get_consume_record();	//返回消费记录
 
 private:
-	Student* _stu;						//所属学生
+
+	int _stu_id;						//所属学生
 	string _password;					//密码
-	int _id, _remain;					//卡号，余额
-	vector <record> _charge, _consume;		//充值，消费记录	
+	int _card_id;
+	double _remain;					//卡号，余额
+	vector<record> _charge, _consume;		//充值，消费记录	
 	bool _flag;							//是否为挂失
 	//卡号，余额，充值记录，消费记录，校园卡状态
 };
 
-vector <Student> students;				//管理员所储存的全校的学生信息
-vector <Card> cards;					//管理员所储存的全校的校园卡信息
+vector<Student> students;				//管理员所储存的全校的学生信息
+vector<Card> cards;					//管理员所储存的全校的校园卡信息
 
 bool create_student();					//管理员创建学生
 
